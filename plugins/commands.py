1ae3a1eb15af61c48847e0c48b84dfd7b2553ef8
ibrahimbadusha296 <51526140+ibrahimbadusha296@users.noreply.github.com>
2023-01-28 06:00:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,6 +244,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+reply_markup=InlineKeyboardMarkup( [[InlineKeyboardButton('Main Channel', url='https://t.me/Tv_Seriessss')] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
